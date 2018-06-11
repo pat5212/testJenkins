ece5e2ab6dcaa830c6fa9bf52cdb7876336ad64a
lcwei <lcwei@tw.ibm.com>
2018-06-11 14:58:40 +0800

    init git

---
+++ src/test/java/com/ibm/jenkinspractice/jenkinspractice/JenkinspracticeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ibm.jenkinspractice.jenkinspractice;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinspracticeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+        	Assert.assertEquals(1,1);
+	}
+
+}
